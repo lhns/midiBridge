49e408a84e78c6770d888e89f5709c9332fdc517
Pierre Kisters <pierrekisters@gmail.com>
2017-01-31 22:38:10 +0100

    testing

--- src/main/scala/org/lolhens/midibridge/Main.scala
+++ src/main/scala/org/lolhens/midibridge/Main.scala
@@ -7,9 +7,9 @@ import javax.sound.midi._
   */
 object Main {
   def main(args: Array[String]): Unit = {
-    val deviceInfos = MidiSystem.getMidiDeviceInfo().toList
+    val deviceInfos = MidiSystem.getMidiDeviceInfo.toList.groupBy(_.getName)
 
-    deviceInfos.foreach { deviceInfo =>
+    deviceInfos.flatMap(_._2).foreach { deviceInfo =>
       println(deviceInfo.getName)
       println("-> " + deviceInfo.getDescription)
       //println("-> " + deviceInfo.getVendor)
@@ -18,12 +18,20 @@ object Main {
       //device.getTransmitter.setReceiver(device.getReceiver)
     }
 
-    def device(info: List[MidiDevice.Info], name: String): List[MidiDevice] = info.filter(_.getName == name).map(MidiSystem.getMidiDevice)
-
-    device(deviceInfos, "APC MINI")(0).getTransmitter.setReceiver(device(deviceInfos, "loopMIDI Port 1" /*"LoopBe Internal MIDI"*/)(1).getReceiver)
+    deviceInfos("Komplete Audio 6 MIDI" /*"APC MINI"*/)(0).openDevice.getTransmitter.setReceiver(
+      deviceInfos(/*"loopMIDI Port 1"*/ "LoopBe Internal MIDI")(1).openDevice.getReceiver)
 
     while (true) {
       Thread.sleep(1000)
     }
   }
+
+  implicit class RichDeviceInfo(val deviceInfo: MidiDevice.Info) extends AnyVal {
+    def openDevice: MidiDevice = {
+      val device = MidiSystem.getMidiDevice(deviceInfo)
+      if (!device.isOpen) device.open()
+      device
+    }
+  }
+
 }
