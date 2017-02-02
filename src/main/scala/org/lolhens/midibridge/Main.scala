15ba3bfd121742fdc4c63cd783416299308b0f2d
LolHens <pierrekisters@gmail.com>
2017-02-02 12:40:12 +0100

    cleanup

--- src/main/scala/org/lolhens/midibridge/Main.scala
+++ src/main/scala/org/lolhens/midibridge/Main.scala
@@ -12,48 +12,6 @@ object Main {
   def main(args: Array[String]): Unit = {
     val midiSystem = MidiSystem()
 
-    val devices = midiSystem.devices.groupBy(_.name)
-
-    devices.flatMap(_._2).foreach { device =>
-      println(device.name)
-      println("-> " + device.description + " t:" + device.isTransmitter + " r:" + device.isReceiver)
-      //println("-> " + deviceInfo.getVendor)
-      //val device = MidiSystem.getMidiDevice(deviceInfo)
-      //if (!device.isOpen) device.open()
-      //device.getTransmitter.setReceiver(device.getReceiver)
-    }
-
-    //val midiLink = devices("Komplete Audio 6 MIDI" /*"APC MINI"*/).flatMap(_.transmitter).head.setReceiver(
-    //  devices(/*"loopMIDI Port 1"*/ "LoopBe Internal MIDI").flatMap(_.receiver).head)
-
-    //midiLink.foreach(_.close())
-
-    //println(devices("Komplete Audio 6 MIDI" /*"APC MINI"*/).flatMap(_.transmitter).head.device)
-
-    //println(link.isDefined)
-
-    /*while (true) {
-      Thread.sleep(1000)
-    }*/
-
     new MidiBridgeGui(midiSystem).main(args)
   }
-
-  /*implicit class RichDeviceInfo(val deviceInfo: MidiDevice.Info) extends AnyVal {
-    def device: MidiDevice = {
-      val device = MidiSystem.getMidiDevice(deviceInfo)
-      device
-    }
-
-    def openDevice: MidiDevice = {
-      val device = deviceInfo.device
-      if (!device.isOpen) device.open()
-      device
-    }
-
-    def isTransmitter: Boolean = deviceInfo.device.getMaxTransmitters != 0
-
-    def isReceiver: Boolean = deviceInfo.device.getMaxReceivers != 0
-  }*/
-
 }
